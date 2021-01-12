81e7792a43664593b39e0ff0afa8b19f69fb6248
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2021-01-12 13:08:31 -0500

    Created and tested nth and length methods.

--- Radix.java
+++ Radix.java
@@ -1,5 +1,27 @@
 public class Radix{
   public static int nth(int n, int col){
-    
+    n = n / (int)Math.pow(10, col);
+    return Math.abs(n % 10);
+  }
+
+  public static int length(int n){
+    if(n == 0){
+      return 1;
+    }
+    else{
+      return (int) (Math.log10(n) + 1);
+    }
+  }
+
+  public static void main(String[]args){
+    System.out.println(nth(123,1));
+    System.out.println(nth(-123,1));
+    System.out.println(nth(123,2));
+    System.out.println(nth(-123,2));
+    System.out.println("-----------");
+    System.out.println(length(2));
+    System.out.println(length(27));
+    System.out.println(length(532));
+    System.out.println(length(1234));
   }
 }
